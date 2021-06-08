4f02a851e8047affe62863e50d7f7217f2ecb1f6
jszukala <burski09@outlook.com>
2021-06-08 15:25:10 +0200

    Working WPF DataGrabber

---
+++ ViewModels/LEDViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApplication2.ViewModels
+{
+    class LEDViewModel
+    {
+    }
+}
---
+++ ViewModels/ParentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace WpfApplication2.ViewModels
+{
+    public class ParentViewModel
+    {
+        ObservableCollection<object> _children;
+        
+        public ParentViewModel()
+        {
+            _children = new ObservableCollection<object>();
+            _children.Add(new MainViewModel());
+            _children.Add(new LEDViewModel());
+        }
+        public ObservableCollection<object> Children { get { return _children; } }
+    }
+}
